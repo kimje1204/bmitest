f069c5aeb95c6cef9b9b0a3bcb0592f0fb644568
쥔니 <138183672+kimje1204@users.noreply.github.com>
2023-08-01 11:28:57 +0900

    Add files via upload

---
+++ bmiController.java
@@ -0,0 +1,47 @@
+package com.example.bmi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class bmiController {
+    @GetMapping("/")
+    public  String bmi() {
+        return "bmi" ;
+    }
+
+    @GetMapping("/bmi_calc")
+    public String bmi_calc(
+            @RequestParam("height") int height,
+            @RequestParam ("weight") int weight,
+            Model model
+    ) {
+        double bmi = weight / Math.pow((double)height/100.0,2) ;
+        String bmiR=String.format("%.1f",bmi);
+
+        if(bmi<20.0) {
+            bmiR = "멸치";
+        } else if (bmi<=24) {
+            bmiR ="정상";
+        } else if (bmi<=29) {
+            bmiR ="과체중";
+        } else if ( bmi>=30) {
+            bmiR = "돼지";
+        }
+
+
+        model.addAttribute("height" ,height);
+        model.addAttribute("weight" ,weight);
+        model.addAttribute("bmiR" ,bmiR);
+        return "bmi";
+    }
+
+
+
+
+
+
+
+}
